d41c2909cd88d3028b89eb91c204bc7550d92b30
Elena Aksenova <lenta4ka2010@mail.ru>
2025-01-08 19:25:36 +0300

    no image

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = 'birthday'
 
-urlpatterns = [
+urlpatterns =[
     path('', views.birthday, name='create'),
-]
+    path('list/', views.birthday_list, name='list'),
+    path('<int:pk>/edit/', views.birthday, name='edit'),
+    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
+    ]
